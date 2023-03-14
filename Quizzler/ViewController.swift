8084982fbfbc97fb3e88f2271abc0d15ced58762
Faiza Nawaz <Faizanawaz755@gmail.com>
2023-03-14 12:11:18 +0500

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Faiza Nawaz on 14/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
